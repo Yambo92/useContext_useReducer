940e14ea0de55670f6071851b683f6d105b75bc9
yambo92 <1810962886@qq.com>
2020-04-22 10:30:16 +0800

    'useReducer'

---
+++ src/reducers/bookReducer.js
@@ -0,0 +1,15 @@
+import {v1 as uuidV1} from 'uuid'
+export const bookReducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_BOOK':
+            return [...state, {
+                title: action.payload.title,
+                author: action.payload.author,
+                id: uuidV1()
+            }];
+        case 'REMOVE_BOOK':
+            return state.filter(book => book.id !== action.payload.id)
+        default:
+            return state;
+    }
+}
